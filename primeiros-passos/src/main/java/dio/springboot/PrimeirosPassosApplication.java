9eea642486349d2ecc8da2d33ae8d172e8cfbbd6
Cassio Ferreira <cassio1215@hotmail.com>
2023-10-18 19:24:30 -0300

    Primeiras classes

--- primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ primeiros-passos/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrimeirosPassosApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
+
 	}
 
 }
