0b86e978e5f3497f0bb4cfcb0ec98bb4e9ad118a
Cassio Ferreira <cassio1215@hotmail.com>
2023-10-18 20:35:05 -0300

    Scopes - Singleton ou Prototype

---
+++ spring-scopes-singleton-prototype/src/main/java/dio/springscopessingletonprototype/app/Beans.java
@@ -0,0 +1,17 @@
+package dio.springscopessingletonprototype.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+@Configuration
+public class Beans {
+    @Bean
+    @Scope("prototype")
+    public Remetente remetente() {
+        System.out.println("CRIANDO OBEJETO REMETENTE");
+        Remetente remetente = new Remetente();
+        remetente.setEmail("noreply@dio.com.br");
+        remetente.setNome("Digital Innovation One");
+        return remetente;
+    }
+}
