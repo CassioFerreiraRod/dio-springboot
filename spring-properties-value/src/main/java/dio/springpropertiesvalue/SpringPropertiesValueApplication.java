0c234cbb3cc3ebd5cf752c1a402f4b55b8085142
Cassio Ferreira <cassio1215@hotmail.com>
2023-10-19 17:09:06 -0300

    Properties Value

---
+++ spring-properties-value/src/main/java/dio/springpropertiesvalue/SpringPropertiesValueApplication.java
@@ -0,0 +1,13 @@
+package dio.springpropertiesvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringPropertiesValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPropertiesValueApplication.class, args);
+	}
+}
